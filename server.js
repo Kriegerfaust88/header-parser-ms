132311466d68019159a2c8b5a35c13f428bc334e
Daniel Schroeder <dschroeder101@gmail.com>
2016-12-08 19:33:57 +0000

    Header information stored in variables

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express')
 var url = require('url')
 var path = require('path')
+var requestLanguage = require('express-request-language')
+var cookieParser = require('cookie-parser')
 
 var app = express();
 
+app.use(cookieParser());
+app.use(requestLanguage({
+    languages: ['en-US', 'zh-CN'],
+    cookie: {
+        name: 'language',
+        options: { maxAge: 24*3600*1000 },
+        url: '/languages/{language}'
+    }
+}));
+
 app.get('/', function(request, response) {
-    response.send('Hello World!')
+    var address = request.connection.remoteAddress
+    var language = request.language
+    var platform = process.platform
+    response.send(address + " " + language + " " + platform)
 })
 
 app.listen(process.env.PORT || 8080, function() {
